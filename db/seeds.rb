0f4fa0f5c9742b477d4b9ea2774a0d65c2f84f7a
Dan Tao <daniel.tao@gmail.com>
2013-07-25 16:39:17 -0700

    Added a seeds.rb script for populating the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dir.glob(File.join(__dir__, 'submissions', '**', '*.md')) do |file|
+  username = File.basename(File.dirname(file))
+
+  user = User.find_by_name(username) || User.create!({
+    :name => username,
+    :email => "#{username.underscore}@rateyourwriting.com",
+    :password => 'passw0rd',
+    :password_confirmation => 'passw0rd'
+  })
+
+  content = File.read(file)
+
+  submission = Submission.create!({
+    :user => user,
+    :title => File.basename(file, '.md'),
+    :body => content
+  })
+
+  puts "Created '#{submission.title}' by #{user.name}."
+end
+
+# For every submission we just created, have most other users cast a random vote
+# for it, with votes for any given submission clustering around a certain value
+# (to emulate submissions of different quality).
+Submission.all.each do |submission|
+  # Let's have votes cluster around anywhere from 2 to 9
+  q = 2 + rand(8)
+
+  User.all(:conditions => ['id != ?', submission.user_id]).each do |user|
+    if rand(100) < 75
+      # Randomly assign a rating between q-1 and q+1
+      rating = (q - 1) + rand(3)
+      user.vote!(submission, rating)
+      puts "#{user.name} rated '#{submission.title}' a #{rating}"
+    end
+  end
+end
