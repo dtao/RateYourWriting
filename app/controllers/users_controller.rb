ef1a93b2042ff535f18c16aa9a96d0af09f1d435
Dan Tao <daniel.tao@gmail.com>
2013-07-25 16:39:17 -0700

    Added view for a single user's submissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @submissions = @user.submissions.all(:order => 'id desc')
+  end
+
   def create
     user = User.create!(user_params)
     alert "Welcome to RateYourWriting, #{user.name}!", :success
--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,4 +1,11 @@
 module SubmissionsHelper
+  def submissions_table(options=nil)
+    options ||= {}
+    render(:partial => 'submissions/table', :locals => {
+      :without => options[:without] || []
+    })
+  end
+
   def submission_body(submission)
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     @markdown.render(submission.body)
