e75fb8a2e6ac3c93d121c481d31e0a254f60ce23
Dan Tao <daniel.tao@gmail.com>
2013-07-25 08:56:49 -0700

    Added User model along with a few RSpec tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe User do
+  let(:attributes) {
+    {
+      :name => 'Dan',
+      :email => 'daniel.tao@gmail.com',
+      :password => 'passw0rd',
+      :password_confirmation => 'passw0rd'
+    }
+  }
+
+  it 'requires a name' do
+    lambda { User.create!(attributes.except(:name)) }.should raise_error
+  end
+
+  it 'prohibits blank names' do
+    lambda { User.create!(attributes.merge(:name => '')) }.should raise_error
+  end
+
+  it 'does not require name to be unique' do
+    User.create!(attributes)
+    User.create!(attributes.merge(:email => 'daniel.tao@yahoo.com'))
+    User.all.should have(2).users
+  end
+
+  it 'requires an e-mail address' do
+    lambda { User.create!(attributes.except(:email)) }.should raise_error
+  end
+
+  it 'prohibits blank e-mail addresses' do
+    lambda { User.create!(attributes.merge(:email => '')) }.should raise_error
+  end
+
+  it 'requires e-mail address to be unique' do
+    User.create!(attributes)
+    lambda { User.create!(attributes) }.should raise_error
+  end
+end
