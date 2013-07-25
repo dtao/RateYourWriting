c70e646179169d493ecfa41bfe10e26faf7b255e
Dan Tao <daniel.tao@gmail.com>
2013-07-25 07:38:16 -0700

    This should be fun!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RateYourWriting::Application.config.secret_key_base = '90f2666b6a009bd2f05dd49d8f3e1b47db09c8f904b4bf4392374976decd5094cef5082f91c3f3fe3fc29d44039895a1ac653bfe09040269acf807efabf1f188'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RateYourWriting::Application.config.session_store :cookie_store, key: '_RateYourWriting_session'
