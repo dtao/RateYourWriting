f89d625a782c8f100a4b8823f77909b4f2e9f18d
Dan Tao <daniel.tao@gmail.com>
2013-07-26 16:43:59 -0700

    Added support for Markdown in comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def submit_link(text, field, value)
     link_to(text, 'javascript:void(0);', :'data-field' => field, :'data-submit' => value)
   end
+
+  def to_markdown(text)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      :disable_indented_code_blocks => true
+    })
+
+    @markdown.render(text)
+  end
 end
--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -6,14 +6,6 @@ module SubmissionsHelper
     })
   end
 
-  def submission_body(submission)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
-      :disable_indented_code_blocks => true
-    })
-
-    @markdown.render(submission.body)
-  end
-
   def voting_section
     render(:partial => 'voting')
   end
