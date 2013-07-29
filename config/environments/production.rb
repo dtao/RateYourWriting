9218cad8474099ce709da506e9c9be2b19acf8cb
Dan Tao <daniel.tao@gmail.com>
2013-07-28 23:59:23 -0700

    Let's just have Rails serve static assets in production for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,8 @@ RateYourWriting::Application.configure do
   }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # TODO: Fix this! (see https://devcenter.heroku.com/articles/rails-integration-gems#serve-static-assets)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
