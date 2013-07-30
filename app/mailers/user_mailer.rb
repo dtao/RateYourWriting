ac049374728bb54888ea07988ce4d444c70fe7f3
Dan Tao <daniel.tao@gmail.com>
2013-07-30 08:53:12 -0700

    Fixed misreferenced constant in UserMailer
    I am an idiot :(

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,10 @@
-require 'env'
-
 class UserMailer < ActionMailer::Base
   def email_verification(user)
     @user = user
 
     mail({
       :to => user.email,
-      :from => "registration@#{Env::HOST}",
+      :from => "registration@#{Env::HTTP_HOST}",
       :subject => 'Welcome to RateYourWriting!'
     })
   end
